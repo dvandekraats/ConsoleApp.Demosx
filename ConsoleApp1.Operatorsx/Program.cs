7b6c90dc5813c36be7c7a9e5dc1197d829e2b3de
Derrick Vandekraats <dvandekraats@outlook.com>
2024-02-11 13:32:05 -0800

    New project on math operators

---
+++ ConsoleApp1.Operatorsx/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.Write("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Add numbers
+int sum = num1 + num2;
+
+// Subtract numbers
+int difference = num1 - num2;
+
+// Multiply numbers
+int product = num1 * num2;
+
+// Divide numbers
+int quotient = num1 / num2;
+
+// Modulus
+int mod = num1 % num2;
+
+Console.WriteLine($"The sum of {num1} + {num2} is: {sum}");
+
+Console.WriteLine($"The difference between {num1} - {num2} is: {difference}");
+
+Console.WriteLine($"The product of {num1} X {num2} is: {product}");
+
+Console.WriteLine($"The quotient of {num1} / {num2} is: {quotient}");
+
+Console.WriteLine($"The modulus of {num1} % {num2} is: {mod}");
+
+
+
+
