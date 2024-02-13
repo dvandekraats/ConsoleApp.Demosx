477cfcf7318ac098aa8c1d96760d21f220c621a9
Derrick Vandekraats <dvandekraats@outlook.com>
2024-02-13 14:22:40 -0800

    Added Try-Catch to sample calculator

--- ConsoleApp1.SampleCalculatorx/Program.cs
+++ ConsoleApp1.SampleCalculatorx/Program.cs
@@ -3,67 +3,78 @@
 Console.WriteLine("************ Welcome to the sample calculator **************");
 
 int choice = 0;
+int num1, num2 = 0;
 while (choice != -1)
 {
-    // Prompt for user input
-
-    // Show calculator options
-    Console.WriteLine("Please select an operation (-1 to exit):");
-    Console.WriteLine("1. Addition (+)");
-    Console.WriteLine("2. Subtraction (-)");
-    Console.WriteLine("3. Multiplication (x)");
-    Console.WriteLine("4. Division (/)");
-    Console.WriteLine("5. Fibonacci sequence");
-    choice = Convert.ToInt32(Console.ReadLine());
-    
-    // exit iteration, if choice is -1 so that numbers are not asked for again
-    if (choice == -1) { continue; };
-    Console.Write("Please enter the first number: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-
-    Console.Write("Please enter the second number: ");
-    int num2 = Convert.ToInt32(Console.ReadLine());
-
-    // Make sure accumulator is reset to zero
-    int answer = 0;
-    switch (choice)
+    try
     {
-        case 1:
-            /* do addition */
-            answer = num1 + num2;
-            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
-            break;
-        case 2:
-            /* do subtraction */
-            answer = num1 - num2;
-            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
-            break;
-        case 3:
-            /* do multiplication */
-            answer = num1 * num2;
-            Console.WriteLine($"{num1} X {num2} = {num1 * num2}");
-            break;
-        case 4:
-            /* do division */
-            answer = num1 / num2;
-            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
-            break;
-        case 5:
-            /* do fibonacci */
+        // Show calculator options
+        Console.WriteLine("Please select an operation (-1 to exit):");
+        Console.WriteLine("1. Addition (+)");
+        Console.WriteLine("2. Subtraction (-)");
+        Console.WriteLine("3. Multiplication (x)");
+        Console.WriteLine("4. Division (/)");
+        Console.WriteLine("5. Fibonacci sequence");
+        choice = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = num1; i <= num2; i++)
-            {
-                answer += i;
-            }
-            Console.WriteLine($"Fibonacci of {num1} to {num2} = {answer}");
-            break;
-        default:
-            Console.WriteLine("Invalid choice, please restart.");
-            break;
+        // exit iteration, if choice is -1 so that numbers are not asked for again
+        if (choice == -1) { continue; };
+        Console.Write("Please enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Please enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        // Make sure accumulator is reset to zero
+        int answer = 0;
+        switch (choice)
+        {
+            case 1:
+                /* do addition */
+                answer = num1 + num2;
+                Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                break;
+            case 2:
+                /* do subtraction */
+                answer = num1 - num2;
+                Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                break;
+            case 3:
+                /* do multiplication */
+                answer = num1 * num2;
+                Console.WriteLine($"{num1} X {num2} = {num1 * num2}");
+                break;
+            case 4:
+                /* do division */
+                answer = num1 / num2;
+                Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                break;
+            case 5:
+                /* do Fibonacci */
+
+                for (int i = num1; i <= num2; i++)
+                {
+                    answer += i;
+                }
+                Console.WriteLine($"Fibonacci of {num1} to {num2} = {answer}");
+                break;
+            default:
+                throw new Exception("Invalid menu item selected");
+       }
     }
-    Console.WriteLine("Press Enter to continue");
-    Console.ReadLine();
-    Console.Clear();
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Cannot divide by zero");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadLine();
+        Console.Clear();
+    }
 }
 
 // print output
