857b9d95c10ff16e9b17da1189e4b1650030976d
Derrick Vandekraats <dvandekraats@outlook.com>
2024-02-13 12:55:04 -0800

    sample program

---
+++ ConsoleApp1.Loopsx/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Simple For Loop Demo
+// Print Hello World 10 times
+
+// 0,1,2,3,4,5,6,7,8,9
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"Hello, World! - {i}");
+//}
+
+//Console.WriteLine("Loop Completed");
+
+// Ask User how many times they wish to print hello world and do so accordingly
+
+//Console.Write("How many times would you like to run the print command? ");
+//int end = Convert.ToInt32(Console.ReadLine());
+//for (int i = 0; i < end; i++)
+//{
+//    Console.WriteLine($"Hello, World! - {i}");
+//}
+
+//Console.WriteLine("Loop Completed");
+
+// While Loop
+
+//int counter = 0;
+//while (counter < 10)
+//{
+//    Console.WriteLine($"Hello, World! - {counter}");
+//    counter++;
+//}
+
+// Ask the user for a number and find the total for each number that is entered. Print
+// final sum when the user enters -1 to exit
+
+//int num = 0;
+//int sum = 0;
+//while (num != -1)
+//{
+//    sum += num;
+//    Console.Write("Enter a number to sum (-1 to end): ");
+//    num = Convert.ToInt32(Console.ReadLine());
+//}
+
+//Console.WriteLine($"Summation is: {sum}");
+
+int num = 0;
+int sum = 0;
+do
+{
+    sum += num;
+    Console.Write("Enter a number to sum (-1 to end): ");
+    num = Convert.ToInt32(Console.ReadLine());
+} while (num != -1);
+
+Console.WriteLine($"The summation is: {sum}");
--- ConsoleApp1.SampleCalculatorx/Program.cs
+++ ConsoleApp1.SampleCalculatorx/Program.cs
@@ -1,82 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Welcome Message
-using System.IO.Pipes;
-
 Console.WriteLine("************ Welcome to the sample calculator **************");
 
-// Prompt for user input
-Console.Write("Please enter the first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Please enter the second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-// Show calculator options
-Console.WriteLine("Please select an operation:");
-Console.WriteLine("1. Addition (+)");
-Console.WriteLine("2. Subtraction (-)");
-Console.WriteLine("3. Multiplication (x)");
-Console.WriteLine("4. Division (/)");
-int choice = Convert.ToInt32(Console.ReadLine());
-
-// Decide which operation is needed based on selected option
-//if(choice == 1)
-//{
-//    /* do addition */
-//    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
-//}
-//else if(choice == 2)
-//{
-//    /* do subtraction */
-//    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
-
-//}
-//else if (choice == 3)
-//{
-//    /* do multiplication */
-//    Console.WriteLine($"{num1} X {num2} = {num1 * num2}");
-
-//}
-//else if (choice == 4)
-//{
-//    /* do division */
-//    Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
-
-//}
-//else
-//{
-//    Console.WriteLine("Invalid choice, please restart.");
-//}
-/* Switch statement */
-int answer = 0;
-switch (choice)
+int choice = 0;
+while (choice != -1)
 {
-    case 1:
-        /* do addition */
-        answer = num1 + num2;
-        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
-        break;
-    case 2:
-        /* do subtraction */
-        answer = num1 - num2;
-        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
-        break;
-    case 3:
-        /* do multiplication */
-        answer = num1 * num2;
-        Console.WriteLine($"{num1} X {num2} = {num1 * num2}");
-        break;
-    case 4:
-        /* do division */
-        answer = num1 / num2;
-        Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
-        break;
-    default:
-        Console.WriteLine("Invalid choice, please restart.");
-        break;
+    // Prompt for user input
+
+    // Show calculator options
+    Console.Clear(); // Clear the screen
+    Console.WriteLine("Please select an operation (-1 to exit):");
+    Console.WriteLine("1. Addition (+)");
+    Console.WriteLine("2. Subtraction (-)");
+    Console.WriteLine("3. Multiplication (x)");
+    Console.WriteLine("4. Division (/)");
+    Console.WriteLine("5. Fibonacci sequence");
+    choice = Convert.ToInt32(Console.ReadLine());
+    
+    // exit iteration, if choice is -1 so that numbers are not asked for again
+    if (choice == -1) { continue; };
+    Console.Write("Please enter the first number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Please enter the second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    
+    switch (choice)
+    {
+        case 1:
+            /* do addition */
+            answer = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            break;
+        case 2:
+            /* do subtraction */
+            answer = num1 - num2;
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            break;
+        case 3:
+            /* do multiplication */
+            answer = num1 * num2;
+            Console.WriteLine($"{num1} X {num2} = {num1 * num2}");
+            break;
+        case 4:
+            /* do division */
+            answer = num1 / num2;
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+            break;
+        case 5:
+            /* do fibonacci */
+
+            // Make sure accumulator is reset to zero
+            int answer = 0;
+
+            for (int i = num1; i <= num2; i++)
+            {
+                answer += i;
+            }
+            Console.WriteLine($"Fibonacci of {num1} to {num2} = {answer}");
+            break;
+        default:
+            Console.WriteLine("Invalid choice, please restart.");
+            break;
+    }
 }
 
 // print output
-Console.WriteLine($"The result is: {answer}");
+Console.WriteLine("Thank you for using the sample calculator");
 
